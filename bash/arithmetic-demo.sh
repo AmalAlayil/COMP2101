29616260f3b5e58b98fed60adcc46be3637ced83
AmalAlayil <56416832+AmalAlayil@users.noreply.github.com>
2020-06-26 00:38:31 -0400

    Update arithmetic-demo.sh

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -8,21 +8,21 @@
 #    the product of the 3 numbers with a label
 
 
-#Asking the user to end 3 numbers.
+#Ask the user to enter 3 numbers.
 echo "Enter any 3 numbers"
 
-#Assigning the number to three variable called firstnum,secondnum and thirdnum.
+#Assigning the num to 3 variable- firstnum,secondnum and thirdnum.
 read firstnum
 read secondnum
 read thirdnum
 
-#Adding the 3 numbers and putting the result into a variable called sum.
+#Adding the 3 num and store the result into a variable called sum.
 sum=$((firstnum + secondnum + thirdnum))
 
-#Finding the product of 3 numbers and putting the result into a variable called product
+#Find the product of 3 numbers and storing the result into a variable called product
 product=$((firstnum * secondnum * thirdnum))
 
-#Editing the output to only show the sum of 3 numbers and their products.
+#Editing the output to show the sum of 3 numbers and their products.
 cat <<EOF
 $firstnum plus $secondnum plus $thirdnum is $sum
 Product of $firstnum , $secondnum and $thirdnum is $product.
